960e3b6f5242073e1b83a2edc9279aa65e37952f
Christian Lagelstorfer <wi19b076@technikum-wien.at>
2021-03-22 12:18:29 +0100

    NAV als Server fertig, NAV als Client noch offen

--- MVVMLightFramework/ViewModel/MainViewModel.cs
+++ MVVMLightFramework/ViewModel/MainViewModel.cs
@@ -1,27 +1,15 @@
+using System.Net;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.CommandWpf;
 using MVVMLightFramework.WebService;
 
 namespace MVVMLightFramework.ViewModel
 {
-    /// <summary>
-    /// This class contains properties that the main View can data bind to.
-    /// <para>
-    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
-    /// </para>
-    /// <para>
-    /// You can also use Blend to data bind with the tool's support.
-    /// </para>
-    /// <para>
-    /// See http://www.galasoft.ch/mvvm
-    /// </para>
-    /// </summary>
     public class MainViewModel : ViewModelBase
     {
 
         public RelayCommand FireClickCommand { get; set; }
         public RelayCommand PromoteClickCommand { get; set; }
-        public bool IsConnected { get; set; } = false;
         private SPS[] _spsArray;
         public SPS[] SpsArray
         {
@@ -29,31 +17,31 @@ namespace MVVMLightFramework.ViewModel
             set { _spsArray = value; RaisePropertyChanged(); }
         }
 
+        public decimal SumAmounts { get; set; }
 
         public MainViewModel()
         {
             SPS_Binding service = new SPS_Binding();
-            service.UseDefaultCredentials = true;
+            service.UseDefaultCredentials = false;
+            service.Credentials=new NetworkCredential("DESKTOP-2K6GVM4\\mckracken", "zak");
 
             SpsArray = service.ReadMultiple(null, null, 100);
             foreach (SPS actSPS in SpsArray)
             {
-
+                SumAmounts += actSPS.Calcd_Current_Value_LCY;
             }
 
             FireClickCommand = new RelayCommand(() =>
             {
-                IsConnected = true;
             }, () =>
             {
-                return !IsConnected;
+                return true;
             });
             PromoteClickCommand = new RelayCommand(() =>
             {
-                IsConnected = true;
             }, () =>
             {
-                return !IsConnected;
+                return true;
             });
         }
     }
